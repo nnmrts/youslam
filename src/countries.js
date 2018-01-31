126fbe3d77e61664db1bc810aa09927b3768560c
Nano Miratus <nanomiratus@gmail.com>
2018-01-31 11:44:12 +0100

    restructured youslam to a class restructured countries to a meta file

---
+++ src/countries.js
@@ -0,0 +1,11 @@
+import AT from "./countries/at.js";
+import DE from "./countries/de.js";
+import IT from "./countries/it.js";
+
+const countries = {
+	AT,
+	DE,
+	IT
+};
+
+export default countries;
--- src/youslam.js
+++ src/youslam.js
@@ -1,33 +1,51 @@
 import assign from "lodash/assign";
 
-import AT from "./countries/at.js";
-import DE from "./countries/de.js";
-import IT from "./countries/it.js";
+import countries from "./countries.js";
 import utils from "./utils.js";
 
+/**
+ * @name YS
+ *
+ * @class YS
+ */
+class YS {
+	/**
+	 * Creates an instance of YS.
+	 * @memberof YS
+	 * @param {object} [filter={path="/"}]
+	 * filter object
+	 */
+	constructor({
+		path = "/",
+		ids
+	} = {}) {
+		Object.keys(countries).forEach((country) => {
+			this[country] = countries[country];
+		});
 
-const youslam = {
-	AT,
-	DE,
-	IT,
-	utils
-};
+		Object.keys(utils).forEach((util) => {
+			this[util] = utils[util];
+		});
 
-youslam.allSlams((
-	country, level1, level2, level3, slam, actualSlam
-) => {
-	actualSlam.id = country + level1 + level2 + level3 + slam;
-	actualSlam.shortId = `${country}-${parseInt(level1, 10)}-${parseInt(level2, 10)}-${parseInt(level3, 10)}-${slam}`;
+		this.allSlams((
+			country, level1, level2, level3, slam, actualSlam
+		) => {
+			actualSlam.id = country + level1 + level2 + level3 + slam;
+			actualSlam.shortId = `${country}-${parseInt(level1, 10)}-${parseInt(level2, 10)}-${parseInt(level3, 10)}-${slam}`;
 
-	actualSlam.location = assign(actualSlam.location, {
-		country,
-		level1: youslam[country][level1].name,
-		level2: youslam[country][level1][level2].name,
-		level3: youslam[country][level1][level2][level3].name,
-		zip: youslam[country][level1][level2][level3].zip
-	});
+			actualSlam.location = assign(actualSlam.location, {
+				country,
+				level1: this[country][level1].name,
+				level2: this[country][level1][level2].name,
+				level3: this[country][level1][level2][level3].name,
+				zip: this[country][level1][level2][level3].zip
+			});
 
-	youslam[country][level1][level2][level3][slam] = actualSlam;
-});
+			this[country][level1][level2][level3][slam] = actualSlam;
+		});
+	}
+}
 
-export default youslam;
+export { YS };
+
+export default new YS();

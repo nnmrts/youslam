54b74ea23d97d47732403a70d289090ae722bd01
Nano Miratus <nanomiratus@gmail.com>
2018-02-16 01:54:06 +0100

    added memberof doc changed it to normal function because this is used removed conditionals

--- src/classes/slam/methods/prototype/get-dates.js
+++ src/classes/slam/methods/prototype/get-dates.js
@@ -5,46 +5,37 @@ import flatten from "lodash/flatten";
 
 /**
  * @name getDates
- *
+ * @memberof Slam
  * @param {number} [amount=3] maximum amount of dates
  * @param {moment} [from=moment()] moment
  * @param {moment} [to=moment().add(100, "y")] moment
  * @returns {array} array of date strings
  */
-const getDates = (amount = -1, from = moment(), to = moment().add(100, "y")) => {
-	if (from.isSameOrBefore(to)) {
-		if (this.dates) {
-			const dateArray = [];
+const getDates = function(amount = -1, from = moment(), to = moment().add(100, "y")) {
+	const dateArray = [];
 
-			Object.keys(this.dates).forEach((year) => {
-				Object.keys(this.dates[year]).forEach((month) => {
-					Object.keys(this.dates[year][month]).forEach((day) => {
-						const date = moment(`${year}-${padStart(month, 2, 0)}-${padStart(day, 2, 0)}`);
+	Object.keys(this.dates).forEach((year) => {
+		Object.keys(this.dates[year]).forEach((month) => {
+			Object.keys(this.dates[year][month]).forEach((day) => {
+				const date = moment(`${year}-${padStart(month, 2, 0)}-${padStart(day, 2, 0)}`);
 
-						if (date.isSameOrAfter(from) && date.isBefore(to)) {
-							dateArray.push(date.format("YYYY-MM-DD"));
-						}
-					});
-				});
+				if (date.isSameOrAfter(from) && date.isBefore(to)) {
+					dateArray.push(date.format("YYYY-MM-DD"));
+				}
 			});
+		});
+	});
 
-			if (dateArray.length > 0) {
-				return slice(flatten(dateArray).sort((dateA, dateB) => {
-					if (moment(dateA).isBefore(moment(dateB))) {
-						return -1;
-					}
-					if (moment(dateA).isAfter(moment(dateB))) {
-						return 1;
-					}
-
-					return 0;
-				}), 0, amount);
-			}
+	return slice(flatten(dateArray).sort((dateA, dateB) => {
+		if (moment(dateA).isBefore(moment(dateB))) {
+			return -1;
 		}
-	}
-	else {
-		throw new Error("[youslam[getDates]]: invalid timespan, \"from\" is after \"to\"");
-	}
+		if (moment(dateA).isAfter(moment(dateB))) {
+			return 1;
+		}
+
+		return 0;
+	}), 0, amount);
 };
 
 export default getDates;

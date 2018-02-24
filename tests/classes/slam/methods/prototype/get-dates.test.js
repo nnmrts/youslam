2ffcb2e6b49e922df12bdf098fa7b57e135e6086
Nano Miratus <nanomiratus@gmail.com>
2018-02-24 22:37:08 +0100

    fixed tests and their titles

--- tests/classes/slam/methods/prototype/get-dates.test.js
+++ tests/classes/slam/methods/prototype/get-dates.test.js
@@ -45,34 +45,41 @@ test("returns an array of 2 objects when no arguments are given", (t) => {
 	t.is(testDates.length, 2);
 });
 
-test("returns an array of 2 objects when \"from\" is set to \"2018-01-01\" and \"to\" is set to \"2018-12-31\"", (t) => {
+test("returns an array of 2 objects when \"from\" is set to \"2018-01-01\"" +
+	"\nand \"to\" is set to \"2018-12-31\"", (t) => {
 	const testDates = testSlam.getDates("2018-01-01", "2018-12-31");
 
 	t.is(testDates.length, 2);
 });
 
-test("returns an array of 1 object when \"from\" is set to \"2018-05-05\" and \"to\" is set to \"2018-12-31\"", (t) => {
+test("returns an array of 1 object when \"from\" is set to \"2018-05-05\"" +
+	"\nand \"to\" is set to \"2018-12-31\"", (t) => {
 	const testDates = testSlam.getDates("2018-05-05", "2018-12-31");
 
 	t.is(testDates.length, 1);
 });
 
-test("returns an array of 1 object when \"from\" is set to \"2018-01-01\", \"to\" is set to \"2018-12-31\" and \"amount\" is set to 1", (t) => {
+test("returns an array of 1 object when \"from\" is set to \"2018-01-01\"," +
+	"\n\"to\" is set to \"2018-12-31\"" +
+	"\nand \"amount\" is set to 1", (t) => {
 	const testDates = testSlam.getDates("2018-01-01", "2018-12-31", 1);
 
 	t.is(testDates.length, 1);
 });
 
-test("returns an empty array when \"from\" is set to \"2018-10-10\", \"to\" is set to \"2018-12-31\"", (t) => {
-	const testDates = testSlam.getDates("2018-01-01", "2018-12-31");
+test("returns an empty array when \"from\" is set to \"2018-10-10\"" +
+	"\nand \"to\" is set to \"2018-12-31\"", (t) => {
+	const testDates = testSlam.getDates("2018-10-10", "2018-12-31");
 
-	t.is(testDates.length, 1);
+	t.is(testDates.length, 0);
 });
 
-test("returns an empty array when \"from\" is set to \"2018-10-10\", \"to\" is set to \"2018-12-31\" and \"amount\" is set to 1", (t) => {
-	const testDates = testSlam.getDates("2018-01-01", "2018-12-31", 1);
+test("returns an empty array when \"from\" is set to \"2018-10-10\"," +
+	"\n\"to\" is set to \"2018-12-31\"" +
+	"\nand \"amount\" is set to 1", (t) => {
+	const testDates = testSlam.getDates("2018-10-10", "2018-12-31", 1);
 
-	t.is(testDates.length, 1);
+	t.is(testDates.length, 0);
 });
 
 test("objects in returned array have the properties slamDate, dateString and moment", (t) => {

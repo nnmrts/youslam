65e328328d2b79397719465b3cd92f633d578d88
Nano Miratus <nanomiratus@gmail.com>
2018-02-24 21:00:40 +0100

    updated test title to a more descriptive one

--- tests/utils/method-adder.test.js
+++ tests/utils/method-adder.test.js
@@ -2,7 +2,7 @@ import test from "ava";
 
 import methodAdder from "../../src/utils/method-adder.js";
 
-test("methodAdder", (t) => {
+test("adds methods and prototype methods to a class", (t) => {
 	const TestClass = class { };
 	const methods = {
 		prototype: {

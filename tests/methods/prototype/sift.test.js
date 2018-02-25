941976bf30e1ddcf73f10e7054deccfc17e74859
Nano Miratus <nanomiratus@gmail.com>
2018-02-25 23:01:30 +0100

    fixed tests for 000 levels

--- tests/methods/prototype/sift.test.js
+++ tests/methods/prototype/sift.test.js
@@ -2,20 +2,20 @@ import test from "ava";
 
 import YS from "../../../src/youslam.js";
 
-test("sifting with \"AT\" results in less or equal to 9 level1s", (t) => {
+test("sifting with \"AT\" results in less or equal to 10 level1s", (t) => {
 	let ys = new YS();
 
 	ys = ys.sift("AT");
 
-	t.true(ys.allLevel1s().length <= 9);
+	t.true(ys.allLevel1s().length <= 10);
 });
 
-test("sifting with \"AT-9\" results in less or equal to 23 level2s", (t) => {
+test("sifting with \"AT-9\" results in less or equal to 24 level2s", (t) => {
 	let ys = new YS();
 
 	ys = ys.sift("AT-9");
 
-	t.true(ys.allLevel2s().length <= 23);
+	t.true(ys.allLevel2s().length <= 24);
 });
 
 test("sifting with \"AT-9-5\" results in less or equal to 1 level3", (t) => {

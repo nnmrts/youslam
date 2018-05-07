5ae0d051e2e6afcee1bac12ed74b1ba28ce8ab3e
Nano Miratus <nanomiratus@gmail.com>
2018-05-07 23:39:34 +0200

    Build: generated dist files
    Signed-off-by: Nano Miratus <nanomiratus@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,18 @@ const pkg = require("./package.json");
 
 taskTime.init();
 
+(function() {
+    var childProcess = require("child_process");
+    var oldSpawn = childProcess.spawn;
+    function mySpawn() {
+        console.log('spawn called');
+        console.log(arguments);
+        var result = oldSpawn.apply(this, arguments);
+        return result;
+    }
+    childProcess.spawn = mySpawn;
+})();
+
 const paths = {
 	dist: "./dist",
 	src: "./src",
